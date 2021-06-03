2eea456aab42278515ed7ddfecf9855514f740b2
Krzysztof Kubicius <kskubicius@gmail.com>
2021-06-03 22:37:27 +0200

    Introduced steganography functions

--- resources/js/interface.js
+++ resources/js/interface.js
@@ -1,59 +1,96 @@
+const steganography = document.querySelector('#steganography');
 
 window.addEventListener('DOMContentLoaded', function (event){
-    const steganography = document.querySelector('#steganography');
-
     if(typeof steganography != undefined){
-        let dataEntry = steganography.querySelector('#data_entry');
-        let input = dataEntry.querySelector('#input_text');
-        showBox(dataEntry, input);
+        steganography.addEventListener('click', function (event){
+            let chooseOption = steganography.querySelector('#choose_option');
+            if(event.target.id == chooseOption.querySelector('#hide').id){
+                event.preventDefault();
+                hideBox(chooseOption);
+                handleHide();
+            }
 
-        let pictureEntry = steganography.querySelector('#picture_entry');
-        let pictureInput = steganography.querySelector('#input_picture');
-        let result = steganography.querySelector('#result');
-
-        steganography.addEventListener('change', function (event){
-            if(event.target.id == input.id) {     
-                let cryptographyObj = new Cryptography();
-                cryptographyObj.setText(event.target.value);
-                let encoded = cryptographyObj.encode();
-                hideBox(dataEntry);
-
-                let encodedInfo = steganography.querySelector('#encoded_info');
-                let encodedInput = encodedInfo.querySelector('#input_encoded');   
-                encodedInput.innerHTML = encoded.encoded;
-                showBox(encodedInfo, encodedInput);
-                window.setTimeout( function() {
-                    hideBox(encodedInfo);
-                    showBox(pictureEntry, pictureInput);
-                }, 5000);
+            if(event.target.id == chooseOption.querySelector('#reveal').id){
+                event.preventDefault();
+                hideBox(chooseOption);
+                handleReveal();
             }
         });
-
-        steganography.addEventListener('input', function (event){
-            if(event.target.id == pictureInput.id){
-                hideBox(pictureEntry);
-                steganographyInput(event, input);
-                showBox(result);
-                let canvas = document.querySelector('#imageCanvas');
-                canvas.style.display = 'block';
-            }
-        });
-
         window.addEventListener("dragover", function(event) {
             event.preventDefault();
             event.dataTransfer.dropEffect = 'copy';
         });
-
-        window.addEventListener("drop", function(event) {
-            event.stopPropagation();
-            event.preventDefault();
-            if (pictureEntry.style.display == 'block') {
-                pictureInput.files = event.dataTransfer.files;
-            }
-        });
     }
 
 });
+
+function handleHide(){
+    let dataEntry = steganography.querySelector('#data_entry');
+    let input = dataEntry.querySelector('#input_text');
+    showBox(dataEntry, input);
+
+    let pictureEntry = steganography.querySelector('#picture_entry');
+    let pictureInput = steganography.querySelector('#input_picture');
+    
+    let encodedInfo = steganography.querySelector('#encoded_info');
+    let encodedInput = encodedInfo.querySelector('#input_encoded');   
+
+    let result = steganography.querySelector('#result');
+
+    steganography.addEventListener('change', function (event){
+        if(event.target.id == input.id) {     
+            let cryptographyObj = new Cryptography();
+            cryptographyObj.setText(event.target.value);
+            let encoded = cryptographyObj.encode();
+            hideBox(dataEntry);
+
+            encodedInput.innerHTML = encoded.encoded;
+            showBox(encodedInfo, encodedInput);
+            window.setTimeout( function() {
+                hideBox(encodedInfo);
+                showBox(pictureEntry, pictureInput);
+            }, 5000);
+        }
+    });
+
+    window.addEventListener("drop", function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        if (pictureEntry.style.display == 'block') {
+            pictureInput.files = event.dataTransfer.files;
+            handleImage('hide', pictureInput, pictureEntry, result, encodedInput.innerHTML);
+        }
+    });
+
+    window.addEventListener('change', function (event){
+        if(event.target.id == pictureInput.id){
+            console.log(event);
+            handleImage('hide', event.target, pictureEntry, result, encodedInput.innerHTML);
+        }
+    });
+}
+
+function handleReveal(){
+    let pictureEntry = steganography.querySelector('#picture_entry');
+    let pictureInput = steganography.querySelector('#input_picture');
+    let result = steganography.querySelector('#result');
+
+    showBox(pictureEntry, pictureInput);
+
+    window.addEventListener('change', function (event){
+        if(event.target.id == pictureInput.id){
+            handleImage('reveal', event, pictureEntry, result);
+        }
+    });
+
+    window.addEventListener("drop", function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        if (pictureEntry.style.display == 'block') {
+            pictureInput.files = event.dataTransfer.files;
+        }
+    });
+}
 
 function showBox(element, input){
     window.setTimeout( function() {
@@ -61,7 +98,9 @@ function showBox(element, input){
         element.classList.add('fadeable');
         window.setTimeout( function() {
             element.className += ' fade-in';
-            input.focus();
+            if(input != null){
+                input.focus();
+            }
         }, 50);
     }, 1000);
 }
@@ -77,4 +116,16 @@ function hideBox(element){
         element.classList.remove('fade-out');
         element.classList.remove('fadeable');
     }, 1000);
+}
+
+function handleImage(mode, pictureInput, pictureEntry, result, input){
+    hideBox(pictureEntry);
+    if(mode == 'hide'){
+        steganographyHide(pictureInput, input);
+    }else if(mode == 'reveal'){
+        steganographyReveal(pictureInput);
+    }
+    showBox(result, null);
+    let canvas = document.querySelector('#imageCanvas');
+    canvas.style.display = 'block';
 }
---
+++ resources/js/steganography.js
@@ -0,0 +1,74 @@
+function steganographyHide(target, input){
+    let canvas = document.getElementById('imageCanvas');
+    let canvasContext = canvas.getContext('2d');
+    let textCanvas = document.getElementById('textCanvas');
+    let textCanvasContext = textCanvas.getContext('2d');
+
+    let reader = new FileReader();
+    reader.addEventListener('load', function(event) {
+        let image = new Image();
+        image.addEventListener('load', function() {
+            canvas.width = image.width;
+            canvas.height = image.height;
+            textCanvas.width = image.width;
+            textCanvas.height = image.height;
+            textCanvasContext.font = "32px Arial";
+            textCanvasContext.fillText(input, 10, 50);
+            canvasContext.drawImage(image, 0, 0);
+            let imageData = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
+            let textData = textCanvasContext.getImageData(0, 0, canvas.width, canvas.height);
+
+            for (let i = 0; i < textData.data.length; i += 4) {
+                if (textData.data[i+3] !== 0) {
+                    if (imageData.data[i+1]%10 != 7 && imageData.data[i+1] > 247) {
+                        imageData.data[i+1] = 247;
+                    }
+                    else {
+                        while (imageData.data[i+1] % 10 != 7) {
+                            imageData.data[i+1]++;
+                        }
+                    }
+                }
+                else {
+                    if (imageData.data[i+1]%10 == 7) {
+                        imageData.data[i+1]--;
+                    }
+                }
+            }
+            canvasContext.putImageData(imageData, 0, 0);
+        });
+        image.src = event.target.result;
+    });
+    reader.readAsDataURL(target.files[0]);
+}
+
+function steganographyReveal(event){
+    let imageCanvas = document.getElementById('imageCanvas');
+    let imageCanvasContext = imageCanvas.getContext('2d');
+
+    let reader = new FileReader();
+    reader.addEventListener('load', function(event) {
+        let image = new Image();
+        image.addEventListener('load', function() {
+            imageCanvas.width = image.width;
+            imageCanvas.height = image.height;
+            imageCanvasContext.drawImage(image, 0, 0);
+
+            let decodeData = imageCanvasContext.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
+            for (let i = 0; i < decodeData.data.length; i += 4) {
+                if (decodeData.data[i+1] % 10 == 7) {
+                    decodeData.data[i] = 0;
+                    decodeData.data[i+1] = 0;
+                    decodeData.data[i+2] = 0;
+                    decodeData.data[i+3] = 255;
+                }
+                else {
+                    decodeData.data[i+3] = 0;
+                }
+            }
+            imageCanvasContext.putImageData(decodeData, 0, 0);
+        });
+        image.src = event.target.result;
+    });
+    reader.readAsDataURL(event.target.files[0]);
+}
